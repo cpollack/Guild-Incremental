81320cdb10c0f4465fee95cbf47bd00583b8f450
cpollack <c_pollack@hotmail.com>
2022-08-03 21:45:02 -0400

    Location Events and tweaks to loc data

---
+++ Guild Incremental/Assets/Resources/LocEvents/LocEventData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocEventData : ScriptableObject
+{
+    public string eventID;
+    public string Name;
+    public string Description;
+ 
+}
